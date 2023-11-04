def5da85326bd46ad874befbe41ce85d3efd25d9
Daniel <danielgadea@users.noreply.github.com>
2023-11-04 12:50:20 +0100

    remove authors as not supported

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #updating the metadata
-exiftool -Title="${INPUT_TITLE}" -Author="${INPUT_AUTHOR}" -Authors="${INPUT_AUTHOR}" -overwrite_original -q "${INPUT_FILE}"
+exiftool -Title="${INPUT_TITLE}" -Author="${INPUT_AUTHOR}" -overwrite_original -q "${INPUT_FILE}"
