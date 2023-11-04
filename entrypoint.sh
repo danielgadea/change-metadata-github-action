b3746cee25861c153eb6b463fd3e331bd5eec77f
Daniel <danielgadea@users.noreply.github.com>
2023-11-04 12:28:31 +0100

    cleanup bash script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,3 @@
 
 #updating the metadata
 exiftool -Title="${INPUT_FILE}" -Author="${INPUT_AUTHOR}" -overwrite_original -q "${INPUT_FILE}"
-message = "Metadata for ${INPUT_FILE} updated successfully!"
-echo "::set-output name=output_message::$message"
